d86b343dfe447c67d813bbccacffa98dfd26dcd9
Zhitenev <azhitenev@alfabank.ru>
2026-05-26 19:08:38 +0400

    first commit

---
+++ src/main/java/ru/vyaly_buhgalter/VyalyBuhgalterApplication.java
@@ -0,0 +1,13 @@
+package ru.vyaly_buhgalter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyalyBuhgalterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VyalyBuhgalterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vyaly_buhgalter/VyalyBuhgalterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vyaly_buhgalter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyalyBuhgalterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
